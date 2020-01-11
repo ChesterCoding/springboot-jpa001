cfad5a338eacbc1669b7e3547e477a51e1c60aa7
ChesterCoding <57189391+ChesterCoding@users.noreply.github.com>
2020-01-11 15:19:51 +0800

    Update SpringbootJpa001Application.java

--- src/main/java/com/offcen/springbootjpa001/SpringbootJpa001Application.java
+++ src/main/java/com/offcen/springbootjpa001/SpringbootJpa001Application.java
@@ -2,7 +2,7 @@ package com.offcen.springbootjpa001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//*********************二次测试 仓库--> 本地********************
 @SpringBootApplication
 public class SpringbootJpa001Application {
 
